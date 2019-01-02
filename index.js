53b128a1539286a907942bdafa6d2227ecd169d6
Farhan Mahmood <farhanm612@gmail.com>
2019-01-03 01:02:31 +0500

    find matching characters

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const input = 'eeyeey';
+const input = 'aeeyeey';
 
 function makePalindrome(input) {
     const charCounts = {};
@@ -10,6 +10,33 @@ function makePalindrome(input) {
     const oddCounts = Object.values(charCounts)
         .filter(count => count % 2 === 1);
     if (oddCounts.length > 1) return -1;
+
+    let cost = 0;
+    for (const currentIndex = 0; currentIndex < input.length / 2; currentIndex++) {
+        const oppositeIndex = input.length - 1 - currentIndex;
+
+        const currentChar = input[currentIndex];
+        const oppositeChar = input[oppositeIndex];
+
+        if (currentChar !== oppositeChar) {
+            let leftIndex = Infinity;
+            let rightIndex = -Infinity;
+
+            for (const i = currentIndex + 1; i < oppositeIndex; i++) {
+                if (input[i] === oppositeChar) {
+                    leftIndex = i;
+                    break;
+                }
+            }
+
+            for (const i = oppositeIndex - 1; i > currentIndex; i--) {
+                if (input[i] === currentChar) {
+                    rightIndex = i;
+                    break;
+                }
+            }
+        }
+    }
 }
 
 console.log(makePalindrome(input));
