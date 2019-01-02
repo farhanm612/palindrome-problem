1e33cda1ef01b738e32c9831611b31f910143a51
Farhan Mahmood <farhanm612@gmail.com>
2019-01-03 00:50:19 +0500

    check for palindrome

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+const input = 'eeyeey';
+
+function makePalindrome(input) {
+    const charCounts = {};
+
+    for (const c of input) {
+        charCounts[c] = (charCounts[c] || 0) + 1
+    }
+
+    const oddCounts = Object.values(charCounts)
+        .filter(count => count % 2 === 1);
+    if (oddCounts.length > 1) return -1;
+}
+
+console.log(makePalindrome(input));
